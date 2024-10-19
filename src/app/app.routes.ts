ac8ca506c5587b8f94484f214427131a803d8e55
slopez <slopezdev1@gmail.com>
2024-10-19 18:05:28 -0300

    se crean las principales rutas con sus componentes-contenedores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/auth',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./routes/auth.routes').then(container => container.routes)
+    },
+    {
+        path: 'managment',
+        loadChildren: () => import('./routes/config.routes').then(container => container.routes)
+    },
+    {
+        path: 'home',
+        loadChildren: () => import ('./routes/home.routes').then(container => container.routes)
+    }
+    
+];
