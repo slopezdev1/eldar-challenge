62db8ee96c454ebbb7dac2f546c4def3af28164c
slopez <slopezdev1@gmail.com>
2024-10-22 01:49:17 -0300

    mockup table mvp

---
+++ src/app/components/header-table/header-table.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-header-table',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './header-table.component.html',
+  styleUrl: './header-table.component.css'
+})
+export class HeaderTableComponent {
+
+}
